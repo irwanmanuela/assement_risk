d2b36e1e30ce5ef3f0bed1419112a204c3d0a287
irwanmanuela <irwanmanuela@susahnyacarinama.com>
2018-10-27 22:51:46 +0700

    migrate data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,99 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('master_data_laporan')->insert([
+            [
+                'laporan_type' => 'PENDAHULUAN',
+                'activity' => 'Mobilisasi Peralatan',
+            ],
+            [
+                'laporan_type' => 'PENDAHULUAN',
+                'activity' => 'Mobilisasi Tenaga Ahli',
+            ],
+            [
+                'laporan_type' => 'PENDAHULUAN',
+                'activity' => 'Mobilisasi Tenaga Pendukung',
+            ],
+            [
+                'laporan_type' => 'PENDAHULUAN',
+                'activity' => 'Menyusun Rencana Kerja',
+            ],
+            [
+                'laporan_type' => 'PENDAHULUAN',
+                'activity' => 'Menyusun Metodologi',
+            ],
+            [
+                'laporan_type' => 'PENDAHULUAN',
+                'activity' => 'Mengumpulkan Data dan Informasi Awal',
+            ],
+            [
+                'laporan_type' => 'PENDAHULUAN',
+                'activity' => 'Menyiapkan Laporan Pendahuluan',
+            ],
+            [
+                'laporan_type' => 'ANTARA',
+                'activity' => 'Survei Observasi Lapangan',
+            ],
+            [
+                'laporan_type' => 'ANTARA',
+                'activity' => 'Survei Instansional',
+            ],
+            [
+                'laporan_type' => 'ANTARA',
+                'activity' => 'Analisis Strukture Ruang',
+            ],
+            [
+                'laporan_type' => 'ANTARA',
+                'activity' => 'Analisis Pola Ruang',
+            ],
+            [
+                'laporan_type' => 'ANTARA',
+                'activity' => 'Analisis Blok Pemanfaatan Ruang',
+            ],
+            [
+                'laporan_type' => 'ANTARA',
+                'activity' => 'Analisis Pelaksanaan Pembangunan Kawasan',
+            ],
+            [
+                'laporan_type' => 'ANTARA',
+                'activity' => 'Perumusan masalah potensi dan peluang',
+            ],
+            [
+                'laporan_type' => 'DRAFT_AKHIR',
+                'activity' => 'Perumusan Tujuan Pengembangan Kawasan',
+            ],
+            [
+                'laporan_type' => 'DRAFT_AKHIR',
+                'activity' => 'Perumusan  Konsep dan strategi pengembangan',
+            ],
+            [
+                'laporan_type' => 'DRAFT_AKHIR',
+                'activity' => 'Perumusan Rencana Strukture dan Pola Ruang',
+            ],
+            [
+                'laporan_type' => 'DRAFT_AKHIR',
+                'activity' => 'Perumusan Pedoman Pelaksanaan Pembangunan',
+            ],
+            [
+                'laporan_type' => 'DRAFT_AKHIR',
+                'activity' => 'Perumusan Mekanisme Pengendalian',
+            ],
+            [
+                'laporan_type' => 'DRAFT_AKHIR',
+                'activity' => 'Menyusun Indikasi Program',
+            ],
+            [
+                'laporan_type' => 'AKHIR',
+                'activity' => 'Penyempurnaan RDTR'
+            ],
+            [
+                'laporan_type' => 'AKHIR',
+                'activity' => 'Penyempurnaan Zoning Regulation'
+            ],
+            [
+                'laporan_type' => 'KHUSUS',
+                'activity' => 'ALBUM PETA'
+            ],
+        ]);
     }
 }
